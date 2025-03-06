ede0bea9fed2cd2cde52fa9fce4abe99b720ff28
HARSH <harshkoli121@gmail.com>
2025-03-06 16:48:10 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
+import { features } from "@/data/features";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,6 +9,29 @@ export default function Home() {
       <div className="grid-background">
       </div>
       <HeroSection />
+
+      <section>
+        <div>
+          <h2>Powerful Features for your Career Growth</h2>
+          <div>{features.map((feature,index)=>{
+            return (
+              <Card>
+                
+                 <CardContent>
+                  <div>{feature.icon}
+                    <h3>{feature.title}</h3>
+                    <p>{feature.description}</p>
+                  </div>
+                </CardContent>
+                
+              </Card>
+
+            );
+          })}
+
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
