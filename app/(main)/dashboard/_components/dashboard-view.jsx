7dfa35eb0a2e797e33eb45db88bad18826bf8aa7
Nirmit18 <nimitkaundalrock2004@gmail.com>
2025-03-10 00:28:43 +0530

    Gemini ai added and more

---
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const DashboardView = (props) => {
+  return (
+    <div>
+      ss
+    </div>
+  )
+};
+
+export default DashboardView;
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/ui/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
+// import { Toaster } from "@/components/ui/sonner";
 
 const inter=Inter({subsets:["latin"]})
 
@@ -29,6 +31,8 @@ export default function RootLayout({ children }) {
           >
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
             <footer className="bg-muted/50 py-12 ">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>lorem</p>
