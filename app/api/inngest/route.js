2355b6d67f28fa964875cf0a168d35624c303765
HARSH <harshkoli121@gmail.com>
2025-03-18 21:55:18 +0530

    interview_quiz

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,13 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
 import { helloWorld } from "./function";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest ,
   functions: [
-    helloWorld,
+    generateIndustryInsights,
   ],
 });
